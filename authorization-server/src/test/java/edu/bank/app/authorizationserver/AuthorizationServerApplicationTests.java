d1d5817181e5a4b315f38446e968614b39b97cde
vega2475 <devid367552@gmail.com>
2025-01-09 22:47:48 +0300

    Implements basic structure with few services

---
+++ authorization-server/src/test/java/edu/bank/app/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bank.app.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ card-service/src/test/java/edu/bank/app/card/CardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bank.app.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
